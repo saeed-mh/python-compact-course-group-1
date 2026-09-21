ccc6afba3348d6247552195b7b258a5a97fb920e
Saeed Mohammadi Gahrooei <saeedmh8908@gmail.com>
2026-09-21 15:13:10 +0200

    Complete week 1 task 2

---
+++ week1/task2.py
@@ -0,0 +1,16 @@
+zahl = 10
+kommazahl = 10.5
+text = "Hello, World!"
+wahrheitswert = True
+
+print("zahl:", zahl)
+print("Type of zahl:", type(zahl))
+
+print("kommazahl:", kommazahl)
+print("Type of kommazahl:", type(kommazahl))
+
+print("text:", text)
+print("Type of text:", type(text))
+
+print("wahrheitswert:", wahrheitswert)
+print("Type of wahrheitswert:", type(wahrheitswert))
