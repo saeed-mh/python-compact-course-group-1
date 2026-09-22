151d9cf270d13f40ef4f4e54e03295015db1792b
Reza Dolatkhah <rzadolatkhah@Gmail.com>
2026-09-22 09:36:54 +0330

    Create task4.py

---
+++ week1/task4.py
@@ -0,0 +1,15 @@
+import math
+
+n = int(input("Enter a number: "))
+print(f"{n}! = {math.factorial(n)}")
+
+
+# Another solution:
+
+#n = int(input("Enter a number: "))
+#factorial = 1
+
+#for i in range(1, n + 1):
+#    factorial *= i
+
+#print(f"{n}! = {factorial}")
