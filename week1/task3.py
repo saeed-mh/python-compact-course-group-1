23e09d15a5af98d2395442188352257d7438fb4c
Reza Dolatkhah <rzadolatkhah@Gmail.com>
2026-09-22 18:08:54 +0330

    Create task3.py
    I did it before the class, to avoid losing time and scores.

---
+++ week1/task3.py
@@ -0,0 +1,14 @@
+integer = 5
+print(float(integer))
+
+floating_point = 5.7
+print(int(floating_point))
+
+integer = 10
+print(str(integer))
+
+number_string = "25"
+print(int(number_string))
+
+integer = 1
+print(bool(integer))
